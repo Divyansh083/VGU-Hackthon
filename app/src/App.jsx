94dc4b2f2d7b22e1b10764cf0575ce6c22440033
Divyansh083 <86774214+Divyansh083@users.noreply.github.com>
2022-10-14 07:47:31 +0530

    landing page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,14 +3,21 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Contact from "./Contact";
 import BuyerDashboard from "./BuyerDashboard";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import LandingPage from "./component/landing";
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
+        <Route path="/" element={<LandingPage/>}/>
         <Route path="/contact" element={<Contact />} />
         <Route path="/buyerdashboard" element={<BuyerDashboard/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }

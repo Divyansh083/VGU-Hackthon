1cccb864d4d066f1dc777863b3b60013a661c494
Divyansh083 <86774214+Divyansh083@users.noreply.github.com>
2022-10-15 17:44:19 +0530

    app.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 }
